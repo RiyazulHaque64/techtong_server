9c8e174c6179bed886293d4036e43d24c677e92f
RiyazulHaque64 <riyazulhaque64@gmail.com>
2024-11-04 15:15:05 +0600

    implement add to cart api

--- src/app/modules/Cart/Cart.services.ts
+++ src/app/modules/Cart/Cart.services.ts
@@ -11,7 +11,56 @@ const addToCart = async (
       id: payload.product_id,
     },
   });
-  return payload;
+
+  let cart = await prisma.cart.findFirst({
+    where: {
+      user_id: user?.id,
+    },
+  });
+  if (!cart) {
+    cart = await prisma.cart.create({
+      data: {
+        user_id: user?.id,
+      },
+    });
+  }
+
+  if (payload.quantity === 0 || (payload.quantity && payload.quantity < 0)) {
+    await prisma.cartItem.deleteMany({
+      where: {
+        cart_id: cart.id,
+        product_id: product.id,
+      },
+    });
+    return null;
+  }
+
+  let cartItem = await prisma.cartItem.findFirst({
+    where: {
+      cart_id: cart.id,
+      product_id: product.id,
+    },
+  });
+  if (cartItem) {
+    cartItem = await prisma.cartItem.update({
+      where: {
+        id: cartItem.id,
+      },
+      data: {
+        quantity: payload.quantity,
+      },
+    });
+  } else {
+    cartItem = await prisma.cartItem.create({
+      data: {
+        cart_id: cart.id,
+        product_id: product.id,
+        quantity: payload.quantity,
+        price: product.price,
+      },
+    });
+  }
+  return cartItem;
 };
 
 export const CartServices = { addToCart };
